63d85a4c35b00853d8c36b924d7266f04176d719
Irvan1998 <38849373+Irvan1998@users.noreply.github.com>
2020-02-10 15:58:05 +0700

    Update _recommended.blade.php

--- resources/views/product/_recommended.blade.php
+++ resources/views/product/_recommended.blade.php
@@ -1,9 +1,9 @@
 <div class="alert alert-warning">
-	<h4 class="font-effect-shadow-multiple" align="center" style="font-family: bebas neue; font-size:30px">Rekomendasi Produk</h4>
+	<h4 class="font-effect-shadow-multiple" align="center" style="font-family: bebas neue; font-size:30px ; color:black">Rekomendasi Produk</h4>
 
 </div>
 <hr />
-<div class="row bg-warning">
+<div class="row ">
 	@foreach ($recommended as $p)
 	<div class="col-md-6">
 		@include('product._single')
